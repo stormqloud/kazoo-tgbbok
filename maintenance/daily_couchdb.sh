ac8254c25d630ef612155a8a29c915d8ce3af7a9
Stormqloud wlloyd <stormqloud@users.noreply.github.com>
2016-03-20 20:11:46 -0400

    Update daily_couchdb.sh

--- maintenance/daily_couchdb.sh
+++ maintenance/daily_couchdb.sh
@@ -1,24 +1,32 @@
 #!/bin/sh
-# Version 0.01
-# To backup BigCouch to Google  Cloud Storage
+# Version 0.02
+
+# To backup BigCouch to Google Cloud Storage
 # stormqloud (wlloyd@prodosec.com)
+
+# Ideally for Kazoo from 2600hz.com!
 
 # This can use d from a daily cronjob oneach bigcouch server.
 
-# Signup for aa  Google Compute Account.  You get 90 days free.
+# Signup for Google Compute Account.  You get 90 days free.
 
 # Under permissions, create a service account
+# You will get a json or pk12 (choose json)
+# rename it service-account.json
+
 # enable google storage api
 # under storage tab, create bucket 
 
 # bucket in this is calles kazoo-backups
 # couchdb is a directory in the bucket that you create with the web interface (or remote it)
 
+# gcloud auth activate-service-account --key-file service-account.json
 
-d=`date +%Y-%m-%d`
+d=`date +%Y%m%d-%H%M`
 h=`hostname`
 f="/tmp/couchdb_${h}_${d}.tgz"
-echo $f
-tar czf ${f} /srv
-gsutil cp ${f} gs://kazoo-backups/couchdb/
- 
+#echo $f
+cd /
+tar -czf ${f} srv
+gsutil -q cp -c ${f} gs://kazoo-backups/couchdb/
+rm ${f}
